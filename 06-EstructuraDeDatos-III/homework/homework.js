2e555e8eca3f121ba7a6ac18946ae20d0b159525
TomyMurphy <tomas.murphyg@gmail.com>
2023-01-16 05:05:09 -0300

    Finished

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -5,13 +5,82 @@
   - size: retorna la cantidad total de nodos del árbol
   - insert: agrega un nodo en el lugar correspondiente
   - contains: retorna true o false luego de evaluar si cierto valor existe dentro del árbol
-  - depthFirstForEach: recorre el árbol siguiendo el orden depth first (DFS) en cualquiera de sus variantes, según se indique por parámetro ("post-order", "pre-order", o "in-order"). Nota: si no se provee ningún parámetro, hará el recorrido "in-order" por defecto.
+  - depthFirstForEach: recorre el árbol siguiendo el orden depth first (DFS) en cualquiera de sus variantes, según se indique por
+    parámetro ("post-order", "pre-order", o "in-order"). Nota: si no se provee ningún parámetro, hará el recorrido "in-order" por
+    defecto.
   - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
-function BinarySearchTree() {}
+function BinarySearchTree(value) {
+   this.value = value;
+   this.right = null;
+   this.left = null;
+}
 
-// No modifiquen nada debajo de esta linea
+BinarySearchTree.prototype.size = function() {
+
+   if (!this.right && !this.left) return 1;
+
+   if (!this.left) return 1 + this.right.size();
+   if (!this.right) return 1 + this.left.size();
+
+   return 1 + this.left.size() + this.right.size();
+}
+
+BinarySearchTree.prototype.insert = function(value) {
+
+   if (value > this.value) {
+      if (!this.right) this.right = new BinarySearchTree(value);
+      else this.right.insert(value);
+   } 
+   else {
+      if (!this.left) this.left = new BinarySearchTree(value);
+      else this.left.insert(value);
+   }
+}
+
+BinarySearchTree.prototype.contains = function(value) {
+
+   if (this.value === value) return true;
+
+   if (value > this.value) {
+      if (!this.right) return false;
+      else return this.right.contains(value);
+   }
+   else {
+      if (!this.left) return false;
+      else return this.left.contains(value);
+   }   
+}
+
+BinarySearchTree.prototype.depthFirstForEach = function(cb, order) {
+
+   if (order === "in-order" || !order) {
+      if (this.left) this.left.depthFirstForEach(cb, order);
+      cb(this.value);
+      if (this.right) this.right.depthFirstForEach(cb, order);
+   } else if (order === "pre-order") {
+      cb(this.value);
+      if (this.left) this.left.depthFirstForEach(cb, order);
+      if (this.right) this.right.depthFirstForEach(cb, order);
+   } else {
+      if (this.left) this.left.depthFirstForEach(cb, order);
+      if (this.right) this.right.depthFirstForEach(cb, order);
+      cb(this.value);
+   }
+}
+
+BinarySearchTree.prototype.breadthFirstForEach = function(cb, array = []) {
+
+   cb(this.value);
+
+   if (this.left) array.push(this.left);
+   if (this.right) array.push(this.right);
+
+   if (array.length) array.shift().breadthFirstForEach(cb, array);
+}
+
+// No modifiquen nada debajo de esta línea
 // --------------------------------
 
 module.exports = {
--- 06-EstructuraDeDatos-III/homework/tests/DataStructureIII.test.js
+++ 06-EstructuraDeDatos-III/homework/tests/DataStructureIII.test.js
@@ -94,7 +94,7 @@ expect(tree.size()).toBe(3);
     expect(testArr).toEqual([ 0, 1, 5, 11, 12, 13, 14, 15, 17, 20, 21, 25, 28, 30, 31, 33, 34, 35, 45, 50 ]);
   });
 
-  // caso de uso: copiar el arbol (procesa primero la raiz)
+  // caso de uso: copiar el árbol (procesa primero la raíz)
   it('corre depth-first (en recorrido "pre-order") cuando depthFirstForEach() es ejecutado con la opcion "pre-order"', function() {
     valuesToInsert.forEach(function(value){
         tree.insert(value);
@@ -103,7 +103,7 @@ expect(tree.size()).toBe(3);
     expect(testArr).toEqual([20, 15, 5, 0, 1, 14, 13, 12, 11, 17, 25, 21, 28, 50, 45, 30, 35, 33, 31, 34]);
   });
 
-  // caso de uso: borrar un arbol, procesa primero las hojas
+  // caso de uso: borrar un árbol, procesa primero las hojas
   it('corre depth-first (en recorrido "post-order" cuando depthFirstForEach() es ejecutado con la opcion "post-order"', function() {
     valuesToInsert.forEach(function(value){
         tree.insert(value);
@@ -112,7 +112,7 @@ expect(tree.size()).toBe(3);
     expect(testArr).toEqual([ 1, 0, 11, 12, 13, 14, 5, 17, 15, 21, 31, 34, 33, 35, 30, 45, 50, 28, 25, 20 ]);
   });
 
-  // util cuando los niveles del arbol tienen significado (org chart? DOM elements?)
+  // útil cuando los niveles del árbol tienen significado (org chart? DOM elements?)
   it('corre breadth-first cuando breadthFirstForEach() es ejecutado', function() {
     valuesToInsert.forEach(function(value){
         tree.insert(value);
